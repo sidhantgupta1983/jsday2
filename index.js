06689db6a7fdb56af082f1d8987932816ee4c977
Sidhant Gupta <sidhantgupta1983@gmail.com>
2022-03-24 21:43:24 +0530

    js Day2

---
+++ index.js
@@ -0,0 +1,68 @@
+// console.log("Working");
+
+// value = 45;//integer type(int/float)
+// // data types - NUmber, String, Boolean
+// //integer float(NUMber)
+
+// // pure number - A number with no decimal values ex = 1,2,45,3564367;
+// //decimal number - A number with decimal values ex = 1.4, 45.50, 
+
+// // int a = 45;
+// // float b = 45.5
+
+// number = 45
+
+// //String - String includes characters, letters, also numbers with the double inverted commas/single inverted commas
+// names = "\Sharad";//string type
+// names1 = "12345";
+// hello = "HEy are you Sharad's Sister?";
+// important = "Sharad 'How are you?' I hope u are doing great";
+// another = 'How are you?';
+// let named = "545, GT ROad, HOwrah - 711101"//characters, letters, numbers
+// console.log(named);
+// let s = "hi all... "+"hope u all"+" doing great.";
+
+// //Boolean - only have two values- 1. true (small letter only) 2. false(small letter only)
+// let isAdult = true;
+// let junior = false;
+
+
+
+// /Whenever we are creating a variable we need to use any of these three keywords
+//1. let  2. const  3. var
+
+// methods to create a variable
+//variable names should be unique;
+//1  = Create a variable;
+//2 = Initaialise a variable with a value;
+// let a; Option1
+let a = "Sharad";//first assignment; Option2 create and initialise a value at the same time.
+// console.log(a);
+// let a = "Ravi";//we cannot have variables with same names
+a = "ravi";//reassigned; "this is reassign and not redeclare"
+console.log(a);
+
+//const
+//1. creation and initialisation has to be done on the same line itself. We cannot assign values to const in later lines
+const b = "Sharad";
+// const b = "Radhe";//we cannot have variables with same names
+//const we also cannot reassign the values;
+console.log(b);
+
+//var keyword
+var c;//creating a variable and then initalize at later lines
+c = "Radhe";//1st initialization/assigning value
+c = "Shyam"  //reassign the value;
+console.log(c);
+var d = "Shyam";//create and initialize the variable in the same line
+console.log(d);
+
+var c = "Krishna";//reusing the variable with the same name
+
+console.log(z);
+
+
+
+
+
+
